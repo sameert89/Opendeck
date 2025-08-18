7476c2717362fc4dd8478203d1616d0614694532
Sameer Trivedi <sameertrivedi1234@gmail.com>
2025-08-18 16:55:08 +0530

    retire winui, switch to cross platform tauri

---
+++ opendeck_host/src/routes/home/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {theme, themes} from "../../lib/stores/theme"
+
+let currentTheme = $derived(themes[$theme]);
+
+</script>
+
+<div class="flex w-full">
+    <div class="flex w-[40%]">
+
+    </div>
+    <div class="flex w-[60%]">
+        
+    </div>
+</div>
